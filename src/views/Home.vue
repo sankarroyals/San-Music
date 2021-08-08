31daf05a2acd6952029edd2d33eba9344dd73ed0
pirate3668 <ysankarmukkesh@outlook.com>
2021-08-08 11:23:26 +0530

    Change in description

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,12 +9,9 @@
     ></div>
     <div class="container mx-auto">
       <div class="text-white main-header-content">
-        <h1 class="font-bold text-5xl mb-5">Listen to Great Music!</h1>
+        <h1 class="font-bold text-5xl mb-5">Sankar Is Welcoming You In This Musical World</h1>
         <p class="w-full md:w-8/12 mx-auto">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et
-          dolor mollis, congue augue non, venenatis elit. Nunc justo eros,
-          suscipit ac aliquet imperdiet, venenatis et sapien. Duis sed magna
-          pulvinar, fringilla lorem eget, ullamcorper urna.
+          Enjoy The Music With Your Friends By uploading And Get Connect With Comments
         </p>
       </div>
     </div>
